5bfc5ce1f686b6d1c342c7016d8bd785e92ceabe
Benjamin Walsh <benjamin.walsh1@dcmail.ca>
2023-12-13 06:41:20 -0500

    Lab 6

---
+++ src/main/java/com/example/servingwebcontent100640803/ServingWebContent100640803Application.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent100640803;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContent100640803Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingWebContent100640803Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servingwebcontent100640803/ServingWebContent100640803ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent100640803;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServingWebContent100640803ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
